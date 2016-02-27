e7ef0c823f279f8a2cad836e2de7ba041ce76a59
freddieiboy <freddieiboy@gmail.com>
2016-02-27 14:29:49 -0800

    make crate pressed

--- app/components/Crate.js
+++ app/components/Crate.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import classNames from 'classnames';
 
 var Crate = React.createClass({
+  getInitialState: function() {
+    return {
+      isPressed: false
+    }
+  },
+  pressCrate: function() {
+    this.setState({isPressed: true})
+  },
   openCrate: function(event) {
     console.log("opened crate: " + this.props.id);
     openCrate(this.props.id);
   },
   render: function() {
+    var crateTop = classNames({
+      'crate-top': !this.state.isPressed,
+      'crate-top-pressed': this.state.isPressed,
+    });
+    var crateBottom = classNames({
+      'crate-bottom': !this.state.isPressed,
+      'crate-bottom-pressed': this.state.isPressed,
+    });
+    var crateShadow = classNames({
+      'crate-shadow': !this.state.isPressed,
+      'crate-shadow-pressed': this.state.isPressed,
+    });
+console.log(this.state.isPressed);
     return (
       <div>
-        <div className="crate-holder" onClick={this.openCrate}>
-          <div className="crate-top"></div>
-          <div className="crate-bottom"></div>
-          <div className="crate-shadow"></div>
+        <div className="crate-holder" onClick={this.openCrate} onMouseDown={this.pressCrate}>
+          <div className={crateTop}></div>
+          <div className={crateBottom}></div>
+          <div className={crateShadow}></div>
         </div>
 
         {this.props.id}
-        
+
       </div>
     );
   }
