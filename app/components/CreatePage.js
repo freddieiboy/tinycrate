c9f1180a89065ea5b0ef26fdc06fc9f45630e8fc
freddieiboy <freddieiboy@gmail.com>
2016-02-28 14:11:20 -0800

    Finish up create.

--- app/components/CreatePage.js
+++ app/components/CreatePage.js
@@ -3,6 +3,8 @@ import { Router, Route, Link, browserHistory } from 'react-router';
 import Autocomplete from 'react-autocomplete';
 import $ from 'jquery';
 import FilePicker from 'component-file-picker';
+
+
 var FIREBASE_URL = "https://crackling-fire-5975.firebaseio.com";
 var ref = new Firebase(FIREBASE_URL);
 var authData = ref.getAuth();
@@ -66,12 +68,12 @@ var CreatePage = React.createClass({
     var newPostRef = postsRef.push();
     var user = ref.getAuth();
     var userRef = ref.child('users').child(user.uid);
-    
+
     var username = $('input[role="combobox"]').val();
     var recipientUser = this.state.users.filter(function(elem) {
       return elem.username === username;
     }).pop();
-    
+
     userRef.once('value', function (snap) {
       var user = snap.val();
       if (!user) {
@@ -107,35 +109,48 @@ var CreatePage = React.createClass({
   render: function() {
     return (
       <div>
-        <div className="homeHeader">
-          <p style={{color: 'white', float: 'right', paddingTop: '10px', paddingBottom: '10px', paddingRight: '15px'}} onClick={this.showHome}>Go Back</p>
+        <div className="homeHeader clearfix" onClick={this.showHome}>
+          <img className="downArrow" src="http://i.imgur.com/cWmUR9n.png" style={{float: 'right', margin: '4px 12px'}}></img>
+          <p style={{color: 'white', float: 'right', paddingTop: '4px', paddingBottom: '10px', paddingRight: '15px'}} >Go Back</p>
         </div>
+
         <div className="container-fluid body-content-create">
-          <div>To: name</div>
-          <Autocomplete
-          initialValue=""
-          items={this.state.users}
-          getItemValue={(item) => item.username}
-          renderItem={(item, isHighlighted) => (
-            <div
-            style={isHighlighted ? styles.highlightedItem : styles.item}
-            key={item.name}
-            id={item.name}
-            >{item.username}</div>
-          )}
-          />
-        </div>
-        <footer>
-        <div className="container" style={{paddingTop: '10px'}}>
-          <div className="row" style={{paddingBottom: '15px'}}>
-          <button style={{position: 'absolute'}} onClick={this.selectFile}>select image</button>
-          <img id="imagePreview" src={'http://www-cdr.stanford.edu/~petrie/blank.gif'} style={{width: '32px', height: '32px', marginLeft: '160px'}} />
+          <form className="toForm">
+            <fieldset>
+              <label className="labelInput">To: name</label>
+              <Autocomplete
+              initialValue=""
+              style={{ display: 'block '}}
+              items={this.state.users}
+              getItemValue={(item) => item.username}
+              renderItem={(item, isHighlighted) => (
+                <div
+                style={isHighlighted ? styles.highlightedItem : styles.item}
+                key={item.name}
+                id={item.name}
+                >{item.username}</div>
+              )}
+              />
+            </fieldset>
+          </form>
+          <div className="outerEmpty createContainerImg">
+            <div className="innerEmpty">
+              <img src="http://i.imgur.com/mfbK1EY.png" alt="" className="emptystate"/>
+            </div>
           </div>
-          <div>
-          <input id="message" placeholder='crate message...' style={{width: '65%'}} onKeyUp={this.handleSendCrateKeyboard}></input>
-          <button style={{marginLeft: '15px'}} onClick={this.handleSendCrateClick}>send</button>
-          </div>
         </div>
+
+        <footer className="footerCreate">
+          <div className="container" style={{paddingTop: '10px'}}>
+            <div className="row" style={{paddingBottom: '15px'}}>
+              <button style={{position: 'absolute'}} onClick={this.selectFile}>select image</button>
+              <img id="imagePreview" src={'http://www-cdr.stanford.edu/~petrie/blank.gif'} style={{width: '32px', height: '32px', marginLeft: '160px'}} />
+            </div>
+            <div>
+              <input id="message" placeholder='crate message...' className="inputSend" style={{width: '65%'}} onKeyUp={this.handleSendCrateKeyboard}></input>
+              <button style={{marginLeft: '15px', float: 'right'}} onClick={this.handleSendCrateClick}>send</button>
+            </div>
+          </div>
         </footer>
       </div>
     );
@@ -147,14 +162,14 @@ let styles = {
     padding: '2px 6px',
     cursor: 'default'
   },
-  
+
   highlightedItem: {
     color: 'white',
     background: 'hsl(200, 50%, 50%)',
     padding: '2px 6px',
     cursor: 'default'
   },
-  
+
   menu: {
     border: 'solid 1px #ccc'
   }
