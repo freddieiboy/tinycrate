c67d6570a46af20ffa340c5a0bbf0b3e93cc0e29
freddieiboy <freddieiboy@gmail.com>
2016-02-28 01:47:25 -0800

    WIP2

--- app/components/InventoryPage.js
+++ app/components/InventoryPage.js
@@ -20,7 +20,7 @@ var InventoryPage = React.createClass({
     var openedCrates = new Firebase(FIREBASE_URL + "/crates");
     openedCratesList = [];
     var user = ref.getAuth();
-    
+
     openedCrates.orderByChild("recipientUId").equalTo(user.uid).on("child_added", function(snapshot) {
       var crate = snapshot.val();
       crate.key = snapshot.key();
@@ -29,7 +29,7 @@ var InventoryPage = React.createClass({
       }
       itself.setState({data:  openedCratesList});
     });
-    
+
     openedCrates.orderByChild("public").equalTo(true).on("child_added", function(snapshot) {
       var crate = snapshot.val();
       crate.key = snapshot.key();
@@ -38,7 +38,7 @@ var InventoryPage = React.createClass({
       }
       itself.setState({data:  openedCratesList});
     });
-  
+
     var userRef;
     userRef = ref.child('users').child(user.uid);
     userRef.once('value', function (snap) {
@@ -55,10 +55,27 @@ var InventoryPage = React.createClass({
   render: function() {
     return (
 
-      <div>
+      <div className="inventory-page-holder">
         <header>
-          <div style={{color: 'white'}}><img className="user-avatar" src={this.state.user.profileImageURL}/>{this.state.user.name}
-          <button style={{float: 'right'}} onClick={this.logout}>Logout</button>
+          <div className="container" style={{height: '100%'}}>
+            <div className="row" style={{height: '100%'}}>
+              <div className="column column-50" style={{height: '100%'}}>
+                <div className="user-avatar-holder" style={{height: '100%'}}>
+                  <img className="user-avatar" src={this.state.user.profileImageURL}/>
+                  {console.log(this.state.user)}
+                </div>
+              </div>
+              <div className="column column-50 user-info-holder">
+                <div className="name float-left">{this.state.user.name}</div>
+              </div>
+              <div className="column column-50 user-info-holder">
+                <div className="username float-left">@{this.state.user.username}</div>
+
+              </div>
+              {/*<div className="column">
+                <button style={{float: 'right'}} onClick={this.logout}>Logout</button>
+              </div>*/}
+            </div>
           </div>
         </header>
         <div className="container-fluid body-content">
