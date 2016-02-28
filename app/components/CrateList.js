2aa1aecf4e1e3c7bad993e3f302db03a158d6fc0
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2016-02-28 01:42:42 -0800

    Added absolute grid for crates

--- app/components/CrateList.js
+++ app/components/CrateList.js
@@ -4,19 +4,9 @@ import Crate from './Crate';
 var CrateList = React.createClass({
   render: function() {
     var onDelete = this.props.onDelete;
-    var crateNodes = this.props.data.map(function(crate) {
-      return (
-        <Crate id={crate.key} onDelete={onDelete}>
-        </Crate>
-      );
-    });
-    return (
-      <div className="crateList" style={{padding: '15px'}}>
-          {crateNodes}
-      </div>
-    );
+    const { item, index, itemsLength } = this.props;
+    return <Crate id={item.key} onDelete={onDelete}></Crate>
   }
 });
-
 
 module.exports = CrateList;
