a4729a3afb0c596f889f6b7d3a9ceada03cc015c
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2016-03-31 22:45:23 -0700

    Added subscriptions/recent crates tabs

--- app/components/ProfilePage.js
+++ app/components/ProfilePage.js
@@ -3,37 +3,67 @@ import Empty from './Empty';
 import ActionBar from './ActionBar';
 import ProfileCrateList from './Crates/ProfileCrateList';
 import ProfileCrate from './Crates/ProfileCrate';
+import CommentList from './CommentList';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import AbsoluteGrid from 'react-absolute-grid';
 var FIREBASE_URL = "https://burning-heat-5122.firebaseio.com";
 var ref = new Firebase(FIREBASE_URL);
+var authData = ref.getAuth();
 var userRef;
-var recentGifteesList = [];
+var openedCratesRef = ref.child('crates');
+var subscriptionsList = [];
+var openedCratesList = [];
+var isMe = false;
+
+var ProfileTabs = {
+  SUBSCRIPTIONS: 0,
+  RECENT_CRATES: 1,
+};
 
 var ProfilePage = React.createClass({
   getInitialState: function() {
     return {
       data: [],
-      user: {}
+      user: {},
+      currentTab: ProfileTabs.SUBSCRIPTIONS
     };
   },
-  subscribe: function(event) {
-    alert('Coming soon!');
+  profileButton: function(event) {
+    if(isMe) {
+      // TODO: show settings page
+      alert("Settings coming soon.");
+    } else {
+      subscribeToUser(this.state.user.username);
+      alert("You are now subscribed to " + this.state.user.username);
+    }
+  },
+  recentCratesTab: function(event) {
+    this.setState({currentTab:  ProfileTabs.RECENT_CRATES});
+  },
+  subscriptionsTab: function(event) {
+    this.setState({currentTab:  ProfileTabs.SUBSCRIPTIONS});
   },
   showCreate: function(event) {
     browserHistory.push("/create");
   },
   componentDidMount: function() {
     var itself = this;
-    var profileFeed = new Firebase(FIREBASE_URL + "/crates");
-    recentGifteesList = [];
+    subscriptionsList = [];
+    openedCratesList = [];
     getUserByUsername(this.props.params.userId, function(user) {
       if (this.isMounted()) {
         itself.setState({user: user});
+        if(this.state.user.uid == authData.uid) {
+          isMe = true;
+          itself.setState({currentTab: ProfileTabs.RECENT_CRATES});
+          getRecentCrates(this.state.user.uid, function() {
+            itself.setState({data:  openedCratesList});
+          });
+        }
       }
       userRef = ref.child('users').child(user.uid);
-      getRecentGiftees(function() {
-        itself.setState({data:  recentGifteesList});
+      getSubscriptions(function() {
+        itself.setState({data:  subscriptionsList});
       });
     }.bind(this));
   },
@@ -51,7 +81,16 @@ var ProfilePage = React.createClass({
     } else {
       emptyState = '';
     }
-    
+
+    var profileTabContent;
+    if(this.state.currentTab == ProfileTabs.SUBSCRIPTIONS) {
+      profileTabContent = 
+      <AbsoluteGrid items={this.state.data} displayObject={(<ProfileCrateList onOpen={this.onOpen} />)} responsive={true} itemHeight={100} itemWidth={92} />
+      {emptyState};
+    } else if(this.state.currentTab == ProfileTabs.RECENT_CRATES) {
+      profileTabContent = <CommentList data={openedCratesList} />;
+    }
+
     return (
       <div className="profile-page-holder">
       <div className="homeHeader" style={styles.homeHeader}>
@@ -80,15 +119,30 @@ var ProfilePage = React.createClass({
               </div>
             </div>
             <div className="column button-holder" style={{height: '100%'}}>
-              <button style={{float: 'right'}} onClick={this.subscribe}>Add Gifter +</button>
+              <button style={{float: 'right'}} onClick={this.profileButton}>
+              {isMe ? 'Settings' : 'Add Gifter +'} 
+              </button>
             </div>
           </div>
         </div>
       </header>
       
         <div style={{padding: '22px'}} className="container-fluid body-content">
-          <AbsoluteGrid items={this.state.data} displayObject={(<ProfileCrateList onOpen={this.onOpen} />)} responsive={true} itemHeight={100} itemWidth={92} />
-          {emptyState}
+        
+        <div className="row">
+        {isMe ?
+          <div className="column" style={{height: '100%'}}>
+            <h5 style={this.state.currentTab == ProfileTabs.RECENT_CRATES ? styles.activeTab : styles.inactiveTab} onClick={this.recentCratesTab}>Recent Crates</h5>
+          </div>
+          : ''
+        }
+          <div className="column">
+            <h5 style={this.state.currentTab == ProfileTabs.SUBSCRIPTIONS ? styles.activeTab : styles.inactiveTab} onClick={this.subscriptionsTab}>Subscriptions</h5>
+          </div>
+        </div>
+
+        {profileTabContent}
+
         </div>
         <ActionBar showCreate={this.showCreate} />
       </div>
@@ -96,10 +150,10 @@ var ProfilePage = React.createClass({
   }
 });
 
-function getRecentGiftees(callback) {
-  userRef.child('giftees').orderByChild('giftedAt').on('child_added', function(snapshot) {
-    getUserByUid(snapshot.key(), function(user) {
-      recentGifteesList.push(user);
+function getSubscriptions(callback) {
+  userRef.child('subscriptions').orderByChild('subscribedAt').on('child_added', function(snapshot) {
+    getUserByUsername(snapshot.key(), function(user) {
+      subscriptionsList.push(user);
       callback();
     });
   });
@@ -111,15 +165,36 @@ function getUserByUsername(username, callback) {
     var key = Object.keys(userObj)[0];
     var user = userObj[key];
     user.uid = key;
+    user.key = key;
     callback(user);
   });
 }
 
-function getUserByUid(uid, callback) {
-  ref.child('users').child(uid).once('value', function(snap) {
-    var user = snap.val();
-    user.key = uid;
-    callback(user);
+// function getUserByUid(uid, callback) {
+//   ref.child('users').child(uid).once('value', function(snap) {
+//     var user = snap.val();
+//     user.key = uid;
+//     callback(user);
+//   });
+// }
+
+function subscribeToUser(username) {
+  userRef.child("subscriptions").child(username).transaction(function(user) {
+    return {
+      subscribedAt: Firebase.ServerValue.TIMESTAMP
+    };
+  });
+}
+
+function getRecentCrates(uid, callback) {
+  openedCratesRef.orderByChild("recipientUId").equalTo(uid).on("child_added", function(snapshot) {
+    var crate = snapshot.val();
+    crate.key = snapshot.key();
+    if(crate.opened === true) {
+      openedCratesList.push(crate);
+    }
+    openedCratesList.reverse();
+    callback();
   });
 }
 
@@ -133,5 +208,11 @@ const styles = {
     top: 0,
     height: 65,
     padding: '15px 22px 38px 28px'
+  },
+  activeTab: {
+    color: '#8F9393'
+  },
+  inactiveTab: {
+    color: '#D7D9DA'
   }
 }
