6737cdaa3947ebecd57a60466ff9671900b5a0fe
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2016-05-14 00:57:52 -0700

    Identify Mixpanel user by Firebase uid

--- src/components/AnalyticsUtil.js
+++ src/components/AnalyticsUtil.js
@@ -1,6 +1,12 @@
+const FIREBASE_URL = "https://burning-heat-5122.firebaseio.com";
+const ref = new Firebase(FIREBASE_URL);
+
 // only track events on Mixpanel in production mode
 export function trackEvent(eventName, properties) {
-  if(process.env.NODE_ENV !== "development") {
+  if(process.env.NODE_ENV === "development") {
+    if(ref.getAuth() !== null) {
+      mixpanel.identify(ref.getAuth().uid);
+    }
     if(properties) {
       mixpanel.track(eventName, properties);
     } else {
