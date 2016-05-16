262d3f205f5ea60c15ac0dfe74764518db8ea5eb
freddieiboy <freddieiboy@gmail.com>
2016-05-15 18:12:40 -0700

    Flex crate on crate list

--- src/components/Crates/CrateList.js
+++ src/components/Crates/CrateList.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Crate from './Crate';
-import CrateTemplate from './CrateTemplate';
+import FlexCrateTemplate from './FlexCrateTemplate';
 
 const CrateList = ({onDelete, item, index, itemsLength, color}) => {
   // const onDelete = this.props.onDelete;
   // const { item, index, itemsLength, color} = this.props;
   // return <Crate msg={item} id={item.key} onDelete={onDelete} color={item.crateColor}></Crate>
   // console.log(item.image)
-  return <CrateTemplate
-    id={item.key}
-    crateSize={80}
-    onDelete={onDelete}
-    color={item.crateColor}
-    cratePreview={item.image}
-    shadow={'true'}
-    crateType={'normal'}
-    popType={'1'}
-    pop={'true'}
-    crateOwnerImage={item.authorProfileImageURL}
-    animation={'animated bounce'}
-    />
+  console.log(item.crateColor)
+  return <FlexCrateTemplate
+      id={item.key}
+      size={80}
+      onDelete={onDelete}
+      color={item.crateColor}
+      preview={item.image}
+      shadow={true}
+      type={'normal'}
+      pop={1}
+      crateOwnerImage={item.authorProfileImageURL}
+      animation={'animated bounce'}
+      />
 }
 
 export default CrateList;
